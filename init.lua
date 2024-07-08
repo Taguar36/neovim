df9c3f248144b14f6c815138a2a638a1a1b77fe1
Taguar <140460754+Taguar36@users.noreply.github.com>
2024-07-08 15:06:41 +1000

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,5 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+vim.o.guifont = "JetBrainsMono Nerd Font:h14" -- text below applies for VimScript
+vim.g.neovide_transparency = 0.8
